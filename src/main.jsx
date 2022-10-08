059a883e46406c9ae635c0b206917227b335f489
Philip Lages <psilvaelages@gmail.com>
2022-10-08 17:02:36 -0300

    feat: add route for the login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './router/Router';
+import Router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-import ErrorPage from '../pages/ErrorPage';
-import Home from '../pages/Home';
+import {  createBrowserRouter,  RouterProvider } from "react-router-dom";
+import ErrorPage from '../pages/errorPage';
+import Home from '../pages/home';
+import Login from '../pages/login';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
     errorElement: <ErrorPage />
+  },
+  {
+    path: "login",
+    element: <Login />
   }
 ])
 
