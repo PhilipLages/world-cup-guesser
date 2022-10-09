496025a2cc291b8b9ee4428756fd2bdd99b4f798
Philip Lages <psilvaelages@gmail.com>
2022-10-09 20:24:28 -0300

    feat: finishes authotization process

--- backend/app/guesses/index.js
+++ backend/app/guesses/index.js
@@ -1,51 +1,60 @@
 import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+
 const prisma = new PrismaClient();
 
 export const createGuess = async ctx => {
-  
-  if(!ctx.request.body.homeTeamScore && !ctx.request.body.awayTeamScore) {
-    ctx.status = 400;
+  if (!ctx.headers.authorization) {
+    ctx.status = 401;
     return;
   }
+  const [type, token] = ctx.headers.authorization.split(" ");
 
-  const { gameId } = ctx.request.body;
-  const homeTeamScore = parseInt(ctx.request.body.homeTeamScore);
-  const awayTeamScore = parseInt(ctx.request.body.awayTeamScore);
-  
   try {
-    const [ guess ]= await prisma.guess.findMany({
-      where: { userId, gameId },
-    });
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+  
+    if(!ctx.request.body.homeTeamScore && !ctx.request.body.awayTeamScore) {
+      ctx.status = 400;
+      return;
+    }
 
-    ctx.body = guess
-    ? await  prisma.guess.update({
-      where: {
-        id: guess.id
-      },
-      data: {
-        homeTeamScore,
-        awayTeamScore,
-      }
-    })
-    : await  prisma.guess.create({
-      data: {
-        userId,
-        gameId,
-        homeTeamScore,
-        awayTeamScore,
-      }
-    })
+    const userId = data.sub;
+    const { gameId } = ctx.request.body;
+    const homeTeamScore = parseInt(ctx.request.body.homeTeamScore);
+    const awayTeamScore = parseInt(ctx.request.body.awayTeamScore);
+    
+    try {
+      const [ guess ]= await prisma.guess.findMany({
+        where: { userId, gameId },
+      });
+
+      ctx.body = guess
+      ? await  prisma.guess.update({
+        where: {
+          id: guess.id
+        },
+        data: {
+          homeTeamScore,
+          awayTeamScore,
+        }
+      })
+      : await  prisma.guess.create({
+        data: {
+          userId,
+          gameId,
+          homeTeamScore,
+          awayTeamScore,
+        }
+      })
+
+    } catch(error) {
+      ctx.body = error;
+      ctx.status = 500;
+    }
 
   } catch(error) {
-    ctx.body = error;
-    ctx.status = 500;
+    ctx.status = 401;
+    return;
   }
-
-
-  // if(exists) {
-  //   // update
-  // } else {
-  //   // create
-  // }
   
 }
--- backend/app/users/index.js
+++ backend/app/users/index.js
@@ -50,6 +50,7 @@ export const login = async (ctx) => {
   const accessToken = jwt.sign({
     sub: user.id,
     name: user.name,
+    expiresIn: "7d",
   }, process.env.JWT_SECRET)
 
   ctx.body = {
