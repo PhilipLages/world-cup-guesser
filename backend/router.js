9416acc2dccdf0a977e2e178ab00317458cd9d81
Philip Lages <psilvaelages@gmail.com>
2022-10-09 17:48:27 -0300

    create prisma structure

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,8 @@
 import Router from  '@koa/router';
 
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+
 export const router = new Router();
 
 const users = []
